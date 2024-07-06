c5a8b2376e2419d5d085954400f50e67edfc7296
Jproga <138982927+Jproga@users.noreply.github.com>
2024-07-06 11:17:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот, который может показывать города на карте. Напиши /help для списка команд.")
+    bot.send_message(message.chat.id, "Привет😀! Я бот, который может показывать города на карте. Напиши /help для списка команд.😉")
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-    bot.send_message(message.chat.id, "Доступные команды:  /show_city, /remember_city, /show_my_cities")
+    bot.send_message(message.chat.id, "Доступные команды😁:  /show_city название города, /remember_city название города,, /show_my_cities")
     # Допиши команды бота
 
 
@@ -30,9 +30,9 @@ def handle_remember_city(message):
     user_id = message.chat.id
     city_name = message.text.split()[-1]
     if manager.add_city(user_id, city_name):
-        bot.send_message(message.chat.id, f'Город {city_name} успешно сохранен!')
+        bot.send_message(message.chat.id, f'Город {city_name} успешно сохранен🥳!')
     else:
-        bot.send_message(message.chat.id, 'Такого города я не знаю. Убедись, что он написан на английском!')
+        bot.send_message(message.chat.id, 'Такого города я не знаю😐. Убедись, что он написан на английском🧐!')
 
 @bot.message_handler(commands=['show_my_cities'])
 def handle_show_visited_cities(message):
@@ -42,7 +42,6 @@ def handle_show_visited_cities(message):
     manager.create_grapf(filename, cities)
     file = open(filename, "rb")
     bot.send_photo(message.chat.id, photo=file)
-
 
 if __name__=="__main__":
     manager = DB_Map(DATABASE)
